af51bf64f520304c4abe3eb4716170c371b91aa5
Santiago <vergarasantiago049@gmail.com>
2025-06-26 23:03:24 -0500

    apps.py documentation updated

--- parking_app/apps.py
+++ parking_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ParqueoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'parking_app'
+    name = 'parking_app' #name of the app, must be the same as the folder name.
