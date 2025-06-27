ef749c2373bbcac2ce83f36b031dbe8df186b5ee
Santiago <vergarasantiago049@gmail.com>
2025-06-26 23:03:59 -0500

    serializers.py documentation updated

--- parking_app/serializers.py
+++ parking_app/serializers.py
@@ -4,7 +4,7 @@ from .models import VehicleRecord
 class VehicleRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = VehicleRecord
-        fields = '__all__'
+        fields = '__all__' #includes all the fields from the model
         read_only_fields = ['total_charge']  #only reading total_charge
 
     def validate(self, data): #validates that exit time isnt earlier than the entry time
@@ -15,18 +15,18 @@ class VehicleRecordSerializer(serializers.ModelSerializer):
             if exit_ < entry:
                 raise serializers.ValidationError("Exit time cannot be earlier than entry time.")
         return data
-
+    
+    #creates method to calculate the total when both entry and exit time are present
     def create(self, validated_data):
         entry = validated_data.get('entry_time')
         exit_ = validated_data.get('exit_time')
 
-        if entry and exit_: #if i have both entries, i calculate the total_charge
+        if entry and exit_: #if i have both entries, it calculate the total_charge
             duration = exit_ - entry
             hours = duration.total_seconds() / 3600
-            validated_data['total_charge'] = round(hours * 2, 2)  #base hourly rate or portion=$2
+            validated_data['total_charge'] = round(hours * 2, 2)  #base hourly rate or portion=$2.00
         else:
-            validated_data['total_charge'] = None
-
+            validated_data['total_charge'] = None #No charge if one of the timestamps is missing 
         return super().create(validated_data)
 
     def update(self, instance, validated_data): #updates every field manually, and keeps values that aren't  provided
@@ -37,10 +37,10 @@ class VehicleRecordSerializer(serializers.ModelSerializer):
         # Recalculate total_charge only if both timestamps are available
 
         if instance.entry_time and instance.exit_time:
-            duration = instance.exit_time - instance.entry_time
+            duration = instance.exit_time - instance.entry_time #total duration 
             hours = duration.total_seconds() / 3600
-            instance.total_charge = round(hours * 2, 2)  
+            instance.total_charge = round(hours * 2, 2)  #the base tax for hour is $2.00 
 
-        instance.save()
+        instance.save() #saves the updated instance to the database
         return instance
 
